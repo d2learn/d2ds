89fedffe7163c81354253ef55ffeb0999289d740
sunrisepeak <speakshen@163.com>
2025-03-22 21:12:15 +0800

    part1

--- videos/array/d2ds_array.py
+++ videos/array/d2ds_array.py
@@ -13,27 +13,84 @@ class D2DSArray(Scene):
 
         contents = DContents([
             "0.基本概念",
-            "1.数组索引",
-            "2.内存布局",
-            "3.操作&应用",
-        ]).arrange(DOWN, aligned_edge=LEFT).to_edge(RIGHT + UP)
+            "1.内存布局",
+            "2.操作&应用",
+        ]).arrange(DOWN, aligned_edge=LEFT).to_corner(UR)
 
-        contents.scale(0.5)
-
-        self.play(Create(contents))
-
-        self.play(contents.anim_next_chapter())
-        self.wait(1)
+        contents.scale(0.45)
 
         # 1. topic enter
-        #self.start_scene()
+        arr = self.start_scene()
         self.wait(1)
 
-        # 
+        self.play(
+            Create(contents),
+            contents.anim_next_chapter()
+        )
 
-    def base(self, array):
+        self.wait(1)
 
-        # index
+        # 2. base concept
+        self.base_concept(arr, contents)
+        self.wait(1)
+
+    def base_concept(self, arr, contents):
+
+        sub_contents_0_1 = Text("下标随机访问").scale(0.8).to_corner(UL)
+        sub_contents_0_2 = Text("相同数据类型").scale(0.8).to_corner(UL)
+        sub_contents_0_3 = Text("连续内存空间").scale(0.8).to_corner(UL)
+
+        self.play(ReplacementTransform(contents[0].copy(), sub_contents_0_1))
+        self.wait(1)
+
+        arr_subscripts = arr.create_subscripts()
+        self.play(Write(arr_subscripts))
+
+        lua_arr_subscripts = arr.create_subscripts(start_index=1)
+        lua_arr_subscripts.next_to(arr_subscripts, DOWN)
+        lua_arr_subscripts.set_color(PURPLE)
+
+        self.play(ReplacementTransform(arr_subscripts.copy(), lua_arr_subscripts))
+        self.wait(1)
+
+        self.play(FadeOut(lua_arr_subscripts))
+
+        sourcecode = """arr[0]"""
+        code = Code(code = sourcecode, language="cpp").move_to(LEFT * 2.5 + UP * 1.5)
+        self.play(Write(code))
+        self.wait(1)
+
+        self.play(
+            DHighlight(arr[0]),
+            DHighlight(arr_subscripts[0]),
+        )
+
+        self.play(
+            DHighlight(arr[4]),
+            DHighlight(arr_subscripts[4])
+        )
+        self.wait(1)
+
+        return
+        # 2. 下标随机访问
+        self.play(
+            ReplacementTransform(contents[0].copy(), sub_contents_0_2),
+            FadeOut(sub_contents_0_1)
+        )
+        self.wait(1)
+
+        sourcecode = """int arr[10] = {0, ... ,9};
+arr[0] = 333; // ok
+arr[0] = "hello"; // error"""
+        code = Code(code = sourcecode, language="cpp").move_to(LEFT * 2.5 + UP * 1.5)
+        self.play(Write(code))
+        self.wait(1)
+
+        # 3. 连续内存空间
+        self.play(
+            ReplacementTransform(contents[0].copy(), sub_contents_0_3),
+            FadeOut(sub_contents_0_2)
+        )
 
         # memory
         return
@@ -47,9 +104,11 @@ class D2DSArray(Scene):
         self.play(Transform(text, arr1))
         self.wait(1)
 
-        arr2 = DArray([i for i in range(10)]).scale(0.5)
-        self.play(Transform(text, arr2))
+        arr2 = DArray([i for i in range(1, 10)]).scale(0.5)
+        self.play(ReplacementTransform(text, arr2))
         self.wait(1)
+
+        return arr2
 
 if __name__ == "__main__":
     scene = D2DSArray()
--- videos/libs/dobject/array.py
+++ videos/libs/dobject/array.py
@@ -6,13 +6,18 @@ class DArray(VGroup):
 
     def __init__(self, elements, **kwargs):
         super().__init__(**kwargs)
-        self.elements = elements
         for i, e in enumerate(elements):
             self.add(DElement(str(e)))
 
         self.arrange(RIGHT)
         self.move_to(ORIGIN)
 
-        # adapt screen size, not too big
-        #if self.get_width() > config["frame_width"] - 10:
-        #    self.set_width(config["frame_width"] - 10)
+    def create_subscripts(self, scale=0.6, start_index = 0, offset=DOWN * 1.5):
+        sub_scripts = VGroup()
+
+        for i in range(len(self)):
+            sub_scripts.add(Text(str(i + start_index)).scale(scale).next_to(self[i], offset))
+
+        sub_scripts.set_color(ORANGE)
+
+        return sub_scripts
